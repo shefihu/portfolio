de74c24cb05046f1a8747429c2fc39a86c9ed78c
shefihu <rotimib0@gmail.com>
2023-01-02 08:54:10 +0100

    animations i hero section

--- src/components/Home2/Hero.jsx
+++ src/components/Home2/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { AiFillGithub, AiFillLinkedin, AiOutlineTwitter } from "react-icons/ai";
 import me from "../../assets/images/home/me.png";
 import { AnimatePresence, motion } from "framer-motion";
-
+import dev from "../../assets/icons/dev.png";
 const Hero = () => {
   return (
     <div>
@@ -34,13 +34,21 @@ const Hero = () => {
               </motion.p>
             </div>
             <div className="w-full h-[20rem] flex justify-center  ">
-              <div className="w-80 h-80 rounded-full bg-white relative">
-                <div className="absolute md:max-w-[7.5rem] max-w-[7.5rem] left-[30%] top-10">
+              <div className="w-80 h-80 rounded-full bg-white flex justify-center overflow-hidden items-center">
+                <div className=" md:max-w-[7.5rem] max-w-[7.5rem] ">
                   <motion.img
                     initial={{ opacity: 0, x: "100px" }}
                     animate={{ opacity: 1, x: 0 }}
                     transition={{ duration: 1 }}
+                    whileTap={{ scale: 1.2 }}
                     src={me}
+                    drag
+                    dragConstraints={{
+                      top: -50,
+                      left: -50,
+                      right: 50,
+                      bottom: 50,
+                    }}
                     alt=""
                     // className="animate-bounce lg:animate-ping"
                   />
@@ -48,7 +56,7 @@ const Hero = () => {
               </div>
             </div>
           </div>
-          <div className="w-10 h-40  fixed 2xl:-left-10 lg:left-5 bottom-0 flex-col space-y-4">
+          <div className="w-10 h-40  fixed 2xl:-left-10 lg:left-5 bottom-10 flex-col space-y-4">
             <div
               href="#_"
               class="rounded-full cursor-pointer w-8 h-8 flex justify-center items-center border-gray-500 border hover:bg-black hover:text-white transition ease-in duration-300"
@@ -67,7 +75,25 @@ const Hero = () => {
             >
               <AiOutlineTwitter className="w-5 h-5" />
             </div>
+            <div
+              href="#_"
+              class="rounded-full cursor-pointer w-8 h-8 flex justify-center items-center border-gray-500 border hover:bg-black hover:text-white transition ease-in duration-300"
+            >
+              <img src={dev} alt="" className="w-5 h-5" />
+            </div>
           </div>
+          <motion.div
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{ duration: 0.5 }}
+            className="w-14 h-14 rounded-full   absolute right-2 bg-gradient-to-r  from-gray-300  top-80"
+          ></motion.div>
+          <motion.div
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{ duration: 0.5 }}
+            className="w-14 h-14 rounded-full   absolute left-2 bg-gradient-to-r  from-gray-300  top-40"
+          ></motion.div>
         </div>
       </AnimatePresence>
     </div>
