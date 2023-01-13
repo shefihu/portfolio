8e974f01ebb4a231be592faa2737f5ecd372be61
shefihu <rotimib0@gmail.com>
2023-01-13 16:09:20 +0100

    about and story

--- src/components/Home2/About.jsx
+++ src/components/Home2/About.jsx
@@ -57,6 +57,60 @@ const About = () => {
             </div>
           </div>
         </div>
+        <div className="w-full h-full lg:flex items-center justify-center">
+          <div className="w-full h-[59rem] max-w-[40rem] relative lg:flex hidden py-10 pt-20 bg-white">
+            <div className="w-full h-full  ">
+              <div className="w-full h-40 ">
+                <motion.h1
+                  initial={{ y: 20, opacity: 0 }}
+                  whileInView={{ y: 0, opacity: 1 }}
+                  transition={{ duration: 0.7 }}
+                  className="large-texts  text-center text-[48px]"
+                >
+                  Yo, Craft your <br />{" "}
+                  <span className=" font-semibold  text-[60px]">
+                    imaginations to life.
+                  </span>
+                </motion.h1>
+                <motion.p
+                  initial={{ y: 20, opacity: 0 }}
+                  whileInView={{ y: 0, opacity: 1 }}
+                  transition={{ duration: 0.8 }}
+                  className="text-[28px]  mt-5 text-center"
+                >
+                  I develop human centered products with unique modern
+                  experiences
+                </motion.p>
+              </div>
+              <div className="w-full h-72  mt-32 flex justify-center">
+                <motion.div
+                  whileInView={{ height: ["0%", "100%"] }}
+                  transition={{ duration: 2, ease: "easeInOut" }}
+                  className="w-[2px] bg-black"
+                ></motion.div>
+              </div>
+              <div className="w-full h-40  mt-6">
+                <motion.h1
+                  initial={{ y: 20, opacity: 0 }}
+                  whileInView={{ y: 0, opacity: 1 }}
+                  transition={{ duration: 0.7 }}
+                  className="large-texts text-center  text-[48px]"
+                >
+                  I build ideas you want <br />
+                  <span className="text-[60px] font-semibold">on the web.</span>
+                </motion.h1>
+                <motion.p
+                  initial={{ y: 20, opacity: 0 }}
+                  whileInView={{ y: 0, opacity: 1 }}
+                  transition={{ duration: 0.8 }}
+                  className="text-[28px] text-center -neue mt-4"
+                >
+                  Strategic driven solutions for the best user experience
+                </motion.p>
+              </div>
+            </div>
+          </div>
+        </div>
       </AnimatePresence>
     </div>
   );
--- src/components/Home2/MyStory.jsx
+++ src/components/Home2/MyStory.jsx
@@ -125,6 +125,100 @@ const MyStory = () => {
           <div className="w-10 h-10 rounded-full   absolute left-32 bg-gradient-to-r  from-gray-300 top-[30rem]"></div>
           <div className="w-10 h-10 rounded-full   absolute right-2 bg-gradient-to-r  from-gray-300 top-[53rem]"></div>
         </div>
+        {/* desktop */}
+        <div className="w-full relative h-full bg-gray-100 py-6  lg:flex hidden">
+          <div className="w-full xl:max-w-[85rem] mx-auto flex justify-between   px-3 h-full pb-3 ">
+            <h1 className="text-[28px] uppercase large-texts mt-4 font-bold">
+              My <br />
+              Story
+            </h1>
+            <div className="w-full max-w-[906px] ">
+              <motion.p
+                variants={variants}
+                initial="initial"
+                whileInView="animate"
+                className="text-[24px] mt-4"
+              >
+                Hello! My name is <span className="font-bold">Shefiu</span>, I
+                enjoy converting ideas into real life products and building
+                solutions that runs on the internet. Over the last 3 years, i've
+                developed products for businesses around the world, providing
+                solutions on small business websites to enteprise web
+                applications with focus on quality, accessibility and fast user
+                experiences.
+              </motion.p>
+              <motion.p
+                variants={variants}
+                initial="initial"
+                whileInView="animate"
+                className="text-[24px] mt-4 "
+              >
+                I have had the great priviledge to work as a Frontend engineer
+                for a Musical agency, an event organizing agency, a finance
+                solution, internal management applications and others.
+                Fast-Forward to today, I work as the lead Frontend engineer at
+                Uniskills, building an accessible digital experience to connect
+                students with businesses that require their services.
+              </motion.p>
+              <motion.p
+                variants={variants}
+                initial="initial"
+                whileInView="animate"
+                className="text-[24px] mt-4"
+              >
+                I believe giving is receiving, so I give back to the Tech
+                ecosystem by building the community, Techers, where individuals
+                building a career in the Tech space support eachother and I
+                share software development articles & resources on my online
+                platforms.
+              </motion.p>
+              <motion.p
+                variants={variants}
+                initial="initial"
+                whileInView="animate"
+                className="text-[24px] mt-4"
+              >
+                What I work with: <br />
+                Languages: Html, Css, Sass, Javascript, Typescript.
+                <br />
+                Technologies: React.js, Next.js, Gatsby.js, SCSS, Tailwind,
+                Firebase, Git, Netlify, Vercel.
+              </motion.p>
+            </div>
+            {/* <div className="w-full h-full flex flex-col mt-10 items-center">
+            <div className="w-[19rem] border-b-2 flex  justify- border-b-white h-2  ">
+              {story.map((project, i) => {
+                let active;
+                let activeText;
+                if (i === index) {
+                  active = "active";
+                  activeText = "activeText";
+                }
+                return (
+                  <div key={i} className=" w-full relative">
+                    <div
+                      onClick={() => {
+                        stor(i);
+                      }}
+                      className={`absolute h-3 w-3 top-[0px] bg-gray-300 rounded-full scale-[0.99] transition ease-in duration-100 ${active} `}
+                    ></div>
+                    <h1 className={`${activeText} pl-10 text-white `}>
+                      {project.projectName}
+                    </h1>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="w-full h-20 mt-14  bg-amber-600">
+              <p>{joinedDetails}</p>
+            </div>
+          </div> */}
+          </div>
+          <div className="w-5 h-5 rounded-full   absolute left-2 bg-gradient-to-r  from-gray-300 "></div>
+          <div className="w-10 h-10 rounded-full   absolute right-2 bg-gradient-to-r  from-gray-300 top-60"></div>
+          <div className="w-10 h-10 rounded-full   absolute left-32 bg-gradient-to-r  from-gray-300 top-[30rem]"></div>
+          <div className="w-10 h-10 rounded-full   absolute right-2 bg-gradient-to-r  from-gray-300 top-[53rem]"></div>
+        </div>
       </AnimatePresence>
     </div>
   );
