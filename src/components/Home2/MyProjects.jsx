c36a0e1ceeffb0d8ab7a0bcb947c5fd9d56894f6
shefihu <rotimib0@gmail.com>
2023-01-26 18:22:29 +0100

    project layout

--- src/components/Home2/MyProjects.jsx
+++ src/components/Home2/MyProjects.jsx
@@ -57,11 +57,97 @@ const MyProjects = () => {
           </div>
         </div>
       </div>
-      <div className="w-full h-full max-w-[95rem] mx-auto py-5 pb-20 text-black bg-white hidden lg:flex">
+      <div className="w-full h-full max-w-[1216px] mx-auto py-5 pb-20 text-black bg-white hidden lg:flex">
         <div className="w-full h-full px-3 pt-8">
           <h1 className="text-[32px] large-texts">My Projects</h1>
           <p>Each one has it's own unique ability</p>
-          <div className="w-full mt-6 grid grid-cols-3  gap-5">
+          <div className="w-full  h-full grid  grid-cols-1 gap-y-16">
+            {projects.map((project, index) => {
+              return (
+                <div
+                  key={index}
+                  className="h-[100%]  [&:nth-child(2n)]:flex-row-reverse items-center flex justify-between  "
+                >
+                  <div className="w-[50%] hover:scale-105 transition-all duration-300 h-[20rem] relative rounded-xl overflow-hidden bg-gray-200">
+                    <div className="absolute backdrop-blur-xs bg-black/20 w-full h-full top-0 "></div>
+                    <img
+                      src={project.image}
+                      alt=""
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+                  <div className="flex-col w-[40%] h-full justify-center space-y-6 mt-5 items-center">
+                    <div className="flex space-x-2 items-end ">
+                      <h1 className="text-lg">{project.id}</h1>
+                    </div>
+                    <h1 className="text-4xl large-texts font-bold">
+                      {project.name}
+                    </h1>
+                    <p className="text-lg">{project.about}</p>
+                    <p className="text-lg mt-2">
+                      Skills/Tools : {project.tools}
+                    </p>
+                    <div className="flex space-x-3">
+                      <a href="f">
+                        <BiLinkExternal className="w-5 h-5" />
+                      </a>
+                      {project.githubLink !== undefined ? (
+                        <a href="f">
+                          <AiFillGithub className="w-5 h-5" />
+                        </a>
+                      ) : (
+                        <></>
+                      )}
+                    </div>
+                  </div>
+                  {/* <div className="w-full h-[20rem] rounded-xl overflow-hidden bg-gray-200">
+                    <img
+                      src={project.image}
+                      alt=""
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+                  <div className="flex justify-between mt-5 items-center">
+                    <div className="flex space-x-2 items-end ">
+                      <h1 className="text-lg">{project.id}</h1>
+                      <h1 className="text-2xl large-texts font-bold">
+                        {project.name}
+                      </h1>
+                    </div>
+                    <div className="flex space-x-3">
+                      <a href="f">
+                        <BiLinkExternal className="w-5 h-5" />
+                      </a>
+                      {project.githubLink !== undefined ? (
+                        <a href="f">
+                          <AiFillGithub className="w-5 h-5" />
+                        </a>
+                      ) : (
+                        <></>
+                      )}
+                    </div>
+                  </div>
+                  <div className="mt-4">
+                    <p className="text-lg">{project.about}</p>
+                    <p className="text-lg mt-2">
+                      Skills/Tools : {project.tools}
+                    </p>
+                  </div> */}
+                </div>
+              );
+            })}
+          </div>
+          <div className=" w-full h-full justify-center flex items-center">
+            <a
+              href="https://github.com/shefihu"
+              className="flex space-x-3 text-xl h-full items-center mt-6"
+            >
+              <span>View more on my Github </span>
+              <BiArrowToRight className="animate-bounce w-6 h-6" />
+            </a>
+          </div>
+
+          {/* <div className="w-full mt-6 grid grid-cols-3  gap-5">
             {projects.map((project, index) => {
               return (
                 <div key={index} className="min-h-[100%]  ">
@@ -110,7 +196,7 @@ const MyProjects = () => {
               <span>View more on my Github </span>
               <BiArrowToRight className="animate-bounce w-6 h-6" />
             </a>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
